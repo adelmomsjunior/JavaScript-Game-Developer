604eccaf9beecbf875ee2938e767d5626d5d1617
theadelmojr <adelmomsjunior@outlook.com>
2021-12-10 12:42:49 -0300

    teste

---
+++ Games/Genius/main.js
@@ -0,0 +1,100 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+const blue = document.querySelector(".blue");
+const red = document.querySelector(".red");
+const green = document.querySelector(".green");
+const yellow = document.querySelector(".yellow");
+
+function shuffleOrder() 
+{
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order) 
+    {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+function lightColor(element, number) 
+{
+    number = number * 500;
+    setTimeout(() => {element.classList.add("selected");}, number - 250);
+    setTimeout(() => {element.classList.remove("selected");});
+}
+
+function checkOrder() {
+    for (let i in clickedOrder) 
+    {
+        if (clickedOrder[i] != order[i]) 
+        {
+            gameOver();
+            break;
+        }
+    }
+    if (clickedOrder.length == order.length) 
+    {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+function click(color) 
+{
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add("selected");
+
+    setTimeout(() => {createColorElement(color).classList.remove("selected");
+                      checkOrder();}, 250);
+}
+
+function createColorElement(color) 
+{
+    switch (color) 
+    {
+        case 0:
+            return green;
+            break;
+        case 1:
+            return red;
+            break;
+        case 2:
+            return yellow;
+            break;
+        case 3:
+            return blue;
+            break;
+    }
+}
+
+function nextLevel() 
+{
+    score++;
+    shuffleOrder();
+}
+
+function gameOver() 
+{
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+    playGame();
+}
+
+function playGame() 
+{
+    alert("Bem vindo ao Gênesis! Iniciando novo jogo!");
+    score = 0;
+    nextLevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JavaScript-Game-Developer
+
+
+<details>
+  <summary>JavaScript Game Developer</summary>
+  <div>
+    <h5>Games</h5>
+    &nbsp; <a href="https://github.com/theadelmojr/Cursos/tree/main/JavaScript%20Game%20Developer/Games/Helicopter">Jogo do helicóptero</a><br/>
+    &nbsp; <a href="https://github.com/theadelmojr/Cursos/tree/main/JavaScript%20Game%20Developer/Games/Genius">Jogo genius</a><br/>
+    &nbsp; <a href="https://github.com/theadelmojr/Cursos/tree/main/JavaScript%20Game%20Developer/Games/Dinosauro">Jogo do dinosauro</a><br/>
+  </div>
+</details>
